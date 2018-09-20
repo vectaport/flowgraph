18c914a0b314eb036c07a9e8914ebd78ee59c2ee
Scott Johnston <johnston@vectaport.com>
2018-09-20 16:31:18 -0500

    migrate from node to hub

--- hub.go
+++ hub.go
@@ -4,34 +4,34 @@ import (
 	"github.com/vectaport/fgbase"
 )
 
-// Node interface for flowgraph nodes that are connected by flowgraph edges
-type Node interface {
+// Hub interface for flowgraph hubs that are connected by flowgraph streams
+type Hub interface {
 	// Tracef for debug trace printing.  Use atomic log mechanism.
 	Tracef(format string, v ...interface{})
 
 	// LogError for logging of error messages.  Use atomic log mechanism.
 	LogError(format string, v ...interface{})
 
-	// Name returns the node name
+	// Name returns the hub name
 	Name() string
 
-	// Source returns upstream edge by index
-	Source(n int) Edge
+	// Source returns upstream stream by index
+	Source(n int) Stream
 
-	// Destination returns downstream edge by index
-	Destination(n int) Edge
+	// Destination returns downstream stream by index
+	Destination(n int) Stream
 
-	// FindSource returns upstream edge by port name
-	FindSource(name string) Edge
+	// FindSource returns upstream stream by port name
+	FindSource(name string) Stream
 
-	// FindDestination returns downstream edge by port name
-	FindDestination(name string) Edge
+	// FindDestination returns downstream stream by port name
+	FindDestination(name string) Stream
 
-	// AddSource adds a source port for each edge
-	AddSource(e ...Edge)
+	// AddSource adds a source port for each stream
+	AddSource(e ...Stream)
 
-	// AddDestination adds a destination port for each edge
-	AddDestination(e ...Edge)
+	// AddDestination adds a destination port for each stream
+	AddDestination(e ...Stream)
 
 	// NumSource returns the number of upstream ports
 	NumSource() int
@@ -51,111 +51,111 @@ type Node interface {
 	// DestinationNames returns the names of the destination ports
 	DestinationNames() []string
 
-	// Base returns the value that implements this node
+	// Base returns the value that implements this hub
 	// The type of this value identifies the implementation.
 	Base() interface{}
 }
 
-// implementation of Node
-type node struct {
+// implementation of Hub
+type hub struct {
 	base *fgbase.Node
 }
 
 // Tracef for debug trace printing.  Uses atomic log mechanism.
-func (n node) Tracef(format string, v ...interface{}) {
+func (n hub) Tracef(format string, v ...interface{}) {
 	n.base.Tracef(format, v)
 }
 
 // LogError for logging of error messages.  Uses atomic log mechanism.
-func (n node) LogError(format string, v ...interface{}) {
+func (n hub) LogError(format string, v ...interface{}) {
 	n.base.LogError(format, v)
 }
 
-// Name returns the node name
-func (n node) Name() string {
+// Name returns the hub name
+func (n hub) Name() string {
 	return n.base.Name
 }
 
-// Source returns upstream edge by index
-func (n node) Source(i int) Edge {
-	return edge{n.base.Srcs[i]}
+// Source returns upstream stream by index
+func (n hub) Source(i int) Stream {
+	return stream{n.base.Srcs[i]}
 }
 
-// Destination returns downstream edge by index
-func (n node) Destination(i int) Edge {
-	return edge{n.base.Dsts[i]}
+// Destination returns downstream stream by index
+func (n hub) Destination(i int) Stream {
+	return stream{n.base.Dsts[i]}
 }
 
-// FindSource returns upstream edge by port name
-func (n node) FindSource(name string) Edge {
+// FindSource returns upstream stream by port name
+func (n hub) FindSource(name string) Stream {
 	e, ok := n.base.FindSrc(name)
 	if !ok {
 		return nil
 	}
 	if e == nil {
-		return edge{nil}
+		return stream{nil}
 	}
-	return edge{e}
+	return stream{e}
 }
 
-// FindDestination returns downstream edge by port name
-func (n node) FindDestination(name string) Edge {
+// FindDestination returns downstream stream by port name
+func (n hub) FindDestination(name string) Stream {
 	e, ok := n.base.FindDst(name)
 	if !ok {
 		return nil
 	}
 	if e == nil {
-		return edge{nil}
+		return stream{nil}
 	}
-	return edge{e}
+	return stream{e}
 }
 
-// AddSource adds a source port for each edge
-func (n node) AddSource(e ...Edge) {
+// AddSource adds a source port for each stream
+func (n hub) AddSource(e ...Stream) {
 	for _, ev := range e {
 		n.base.Srcs = append(n.base.Srcs, ev.Base().(*fgbase.Edge))
 	}
 }
 
-// AddDestination adds a destination port for each edge
-func (n node) AddDestination(e ...Edge) {
+// AddDestination adds a destination port for each stream
+func (n hub) AddDestination(e ...Stream) {
 	for _, ev := range e {
 		n.base.Dsts = append(n.base.Dsts, ev.Base().(*fgbase.Edge))
 	}
 }
 
 // NumSource returns the number of upstream ports
-func (n node) NumSource() int {
+func (n hub) NumSource() int {
 	return len(n.base.Srcs)
 }
 
 // NumDestination returns the number of downstream ports
-func (n node) NumDestination() int {
+func (n hub) NumDestination() int {
 	return len(n.base.Dsts)
 }
 
 // SetSourceNames names the source ports
-func (n node) SetSourceNames(nm ...string) {
+func (n hub) SetSourceNames(nm ...string) {
 	n.base.SetSrcNames(nm...)
 }
 
 // SetDestinationNames names the destination ports
-func (n node) SetDestinationNames(nm ...string) {
+func (n hub) SetDestinationNames(nm ...string) {
 	n.base.SetDstNames(nm...)
 }
 
 // SourceNames returns the names of the source ports
-func (n node) SourceNames() []string {
+func (n hub) SourceNames() []string {
 	return n.base.SrcNames()
 }
 
 // DestinationNames returns the names of the destination ports
-func (n node) DestinationNames() []string {
+func (n hub) DestinationNames() []string {
 	return n.base.DstNames()
 }
 
-// Base returns the value that implements this node
+// Base returns the value that implements this hub
 // The type of this value identifies the implementation.
-func (n node) Base() interface{} {
+func (n hub) Base() interface{} {
 	return n.base
 }
