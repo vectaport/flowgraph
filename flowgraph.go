e30a30110b846c9024c4cb3f085c9bd6922881de
Scott Johnston <johnston@vectaport.com>
2018-11-03 12:40:47 -0700

    migrate to HubCode

--- flowgraph.go
+++ flowgraph.go
@@ -66,7 +66,7 @@ const (
 
 // Transformer transforms a slice of source values into a slice
 // of result values with the Transform method. Provide as init arg to
-// NewHub with AllOf or OneOf HubCode or optionally to a Hub with a logic or
+// NewHub with AllOf or OneOf HubCode or optionally with a logic or
 // math HubCode. Use Hub.Tracef for tracing.
 type Transformer interface {
 	Transform(h Hub, source []interface{}) (result []interface{}, err error)
